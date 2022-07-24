d349868f1ea9b911eda670a41a9e123626c92bc8
MohamadZaman Kazemnejad <40980064+M-Kazemnejad@users.noreply.github.com>
2022-07-24 10:43:50 +0430

    added static files

--- website/urls.py
+++ website/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 from .views import *
 
@@ -7,3 +9,6 @@ urlpatterns = [
     path('contact/', contact_view),
     
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
