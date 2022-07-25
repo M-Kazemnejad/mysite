afb073eeea038d142a5fa1d367f1bb04391e2127
MohamadZaman Kazemnejad <40980064+M-Kazemnejad@users.noreply.github.com>
2022-07-25 20:01:30 +0430

    edited blog model

--- blog/models.py
+++ blog/models.py
@@ -10,7 +10,7 @@ class Post(models.Model):
     # tag
     # category
     counted_views = models.IntegerField(default=0)
-    status = models.BooleanField()
-    published_date = models.DateTimeField()
+    status = models.BooleanField(default=False)
+    published_date = models.DateTimeField(null=True)
     created_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
