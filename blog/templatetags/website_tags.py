9d85a3b54f138c1ad744a3df9f1a84f1096d8022
MohamadZaman Kazemnejad <40980064+M-Kazemnejad@users.noreply.github.com>
2022-08-06 00:47:24 +0430

    index carousel added

--- blog/templatetags/website_tags.py
+++ blog/templatetags/website_tags.py
@@ -3,3 +3,7 @@ from blog.models import Post, Category
 
 register = template.Library()
 
+@register.inclusion_tag('website/index-recent-post.html')
+def recent_post(arg=6):
+    posts = Post.objects.filter(status=1).order_by('-published_date')[:arg]
+    return {'posts':posts}
